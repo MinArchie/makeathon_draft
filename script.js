ad4c4412be72dea5c399838e95ffd016d7fd0100
swathi <swathi.shrirang.kulkarni@gmail.com>
2024-01-05 17:56:21 +0530

    Added new files, fixed some media query stuff

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let valley3 = document.getElementById('valley3');
 let birds = document.getElementById('birds');
 let button = document.getElementById('button');
 
+
+
 function updateParallax() {
     let value = window.scrollY;
     let opacity = 1 - value / 500; // Adjust the divisor for the fade-out speed
@@ -18,12 +20,12 @@ function updateParallax() {
         button.style.opacity = opacity;
 
         sun.style.transform = `translate(0, ${value * 0.5}px)`;
-        valley3.style.transform = `translate(0, ${value * 0.3}px)`;
-        valley2.style.transform = `translate(0, ${value * 0.3}px)`;
-        valley1.style.transform = `translate(0, ${value * 0.3}px)`;
+        valley3.style.transform = `translate(0, ${value * 0.1}px)`;
+        valley2.style.transform = `translate(0, ${value * 0.2}px)`;
+        valley1.style.transform = `translate(0, ${value * 0.2}px)`;
         sky.style.transform = `translate(0, ${value * 0.2}px)`;
         birds.style.transform = `translate(${value * -0.5}px, 0)`;
-    } else if (window.innerWidth < 991 && window.innerWidth > 769) {
+    } else if (window.innerWidth < 991 && window.innerWidth > 449) {
         sun.style.transform = `translate(0, ${value * 0.5}px)`;
         valley3.style.transform = `translate(0, ${value * 0.3}px)`;
         valley2.style.transform = `translate(0, ${value * 0.3}px)`;
